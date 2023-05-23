bb579dc68dedd726f3e8ddd321c07d53955fb306
Elodie Daubie <elodie.daubie@gmail.com>
2023-05-23 12:33:26 +0200

    add queries and mutations

--- src/CountryResolver.ts
+++ src/CountryResolver.ts
@@ -1,9 +1,29 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import Country, { CountryInput } from "./entities/Country";
+import datasource from "./db";
 
 @Resolver()
 export class CountryResolver {
   @Query(() => String)
   async hello() {
     return "Hello, World!";
+  }
+  @Mutation(() => Country)
+  async createCountry(@Arg("data") data: CountryInput): Promise<Country> {
+    return await datasource.getRepository(Country).save(data);
+  }
+
+  @Query(() => [Country])
+  async getCountries(): Promise<Country[]> {
+    return await datasource.getRepository(Country).find();
+  }
+
+  @Query(() => Country)
+  async getCountry(@Arg("code") code: string): Promise<Country> {
+    const country = await datasource.getRepository(Country).findOneBy({ code });
+
+    if (country === null) throw new Error("Country not found");
+
+    return country;
   }
 }
